3999497b1e91c1fa79416262fd14a0fb4e633d7e
Your Name <you@example.com>
2024-03-02 15:54:38 -0500

    .

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const CoadminService = require('./models/CoadminService.js')
 const ReportIssues = require('./models/ReportIssues.js')
-const Tests = require('./models/Tests.js')
 
 module.exports = {
     CoadminService,
